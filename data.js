a580b9c653d15176410505a05253fca975a9fe71
LilyN11 <53539773+LilyN11@users.noreply.github.com>
2020-12-09 21:35:44 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,137 @@
+// I store all the src names of my audio file along with the english text 
+var myarray = [
+	{
+		"question": "audio/New%20Recording%201.m4a",
+		"response": "What is the form of government of the United States?"
+	},
+	{
+		"question": "audio/New%20Recording%202.m4a",
+		"response": "What is the supreme law of the land?"
+	},
+	{
+		"question": "audio/New%20Recording%203.m4a",
+		"response": "Name one thing the U.S. Constitution does?"
+	},
+	{
+		"question": "audio/New%20Recording%204.m4a",
+		"response":"The U.S. Constitution starts with the words we the people. What does we the people mean?"
+	},
+	{
+		"question":"audio/New%20Recording%205.m4a",
+		"response":"How are changes made to the U.S. Constitution?"
+	},
+    {
+		"question":"audio/New%20Recording%206.m4a",
+		"response":"What does the Bill of Rights protect?"
+	}, 
+	{
+		"question":"audio/New%20Recording%207.m4a",
+		"response":"How many amendments does the U.S. Constitution have? "
+	},
+	{
+		"question":"audio/New%20Recording%208.m4a",
+		"response":"Why is the Declaration of Independence important?"
+	},
+	{
+		"question":"audio/New%20Recording%209.m4a",
+		"response":"What founding document said the American colonies were free from Britain?"
+	},
+	{
+		"question":"audio/New%20Recording%2010.m4a",
+		"response":"Name two important ideas from the Declaration of Independence and the U.S. Constitution."
+	},
+    {
+		"question": "audio/New%20Recording%2011.m4a",
+		"response": "The words life, liberty, and the pursuit of happiness are in what founding document?"
+	},
+	{
+		"question": "audio/New%20Recording%2012.m4a",
+		"response": "What is the economic system of the United States?"
+	},
+	{
+		"question": "audio/New%20Recording%2013.m4a",
+		"response": "What is the rule of law?"
+	},
+	{
+		"question": "audio/New%20Recording%2014.m4a",
+		"response":"Many documents influenced the U.S. Constitution. Name one."
+	},
+	{
+		"question":"audio/New%20Recording%2015.m4a",
+		"response":"There are three branches of government. Why?"
+	},
+    {
+		"question":"audio/New%20Recording%2016.m4a",
+		"response":"Name the three branches of government."
+	},
+    {
+		"question":"audio/New%20Recording%2017.m4a",
+		"response":"The President of the United States is in charge of which branch of government?"
+	},
+    {
+		"question":"audio/New%20Recording%2018.m4a",
+		"response":"What part of the federal government writes laws?"
+	},
+    {
+		"question":"audio/New%20Recording%2019.m4a",
+		"response":"What are the two parts of the U.S. Congress?"
+	},
+    {
+		"question":"audio/New%20Recording%2020.m4a",
+		"response":"Name one power of the U.S. Congress. "
+	},
+    {
+		"question":"audio/New%20Recording%2021.m4a",
+		"response":"How many U.S. senators are there?"
+	},
+    {
+		"question":"audio/New%20Recording%2022.m4a",
+		"response":"How long is a term for a U.S. senator?"
+	},
+    {
+		"question":"audio/New%20Recording%2023.m4a",
+		"response":"Who is one of your state’s U.S. senators now?"
+	},
+    {
+		"question":"audio/New%20Recording%2024.m4a",
+		"response":"How many voting members are in the House of Representatives?"
+	},
+    {
+		"question":"audio/New%20Recording%2025.m4a",
+		"response":"How long is a term for a member of the House of Representatives?"
+	},
+    {
+		"question":"audio/New%20Recording%2026.m4a",
+		"response":"Why do U.S. representatives serve shorter terms than U.S. senators?"
+	},
+    {
+		"question":"audio/New%20Recording%2027.m4a",
+		"response":"How many senators does each state have?"
+	},
+    {
+		"question":"audio/New%20Recording%2028.m4a",
+		"response":"Why does each state have two senators?"
+	},
+    {
+		"question":"audio/New%20Recording%2029.m4a",
+		"response":"Name your U.S. representative. "
+	},
+    {
+		"question":"audio/New%20Recording%2030.m4a",
+		"response":"What is the name of the speaker of the House of Representatives now? "
+	},
+    {
+		"question":"audio/New%20Recording%2031.m4a",
+		"response":"Who does a U.S. senator represent?"
+	},
+    {
+		"question":"audio/New%20Recording%2032.m4a",
+		"response":"Who elects U.S. senators?"
+	},
+    {
+		"question":"audio/New%20Recording%2033.m4a",
+		"response":"Who does a member of the house of representatives represent?"
+	}
+	
+]
+
